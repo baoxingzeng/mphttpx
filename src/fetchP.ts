abc2e80f708fba8a78c7a127202fde277b8c4086
Xingzeng Bao <baoxingzeng@gmail.com>
2026-07-26 01:40:43 +0800

    tree-shaking optimization

--- src/fetchP.ts
+++ src/fetchP.ts
@@ -1,13 +1,13 @@
 import { XMLHttpRequest } from "miniprogram-xmlhttprequest-shim";
 import { fetch, fetchP, setXMLHttpRequestClass } from "fetch-xhr-shim";
 
-const fetches = (function () {
+const fetches = /*#__PURE__*/function () {
     setXMLHttpRequestClass(XMLHttpRequest);
     return [fetch, fetchP] as const;
-})();
+}();
 
-const fetchE = fetches[0];
-const fetchPolyfill = fetches[1];
+const fetchE = /*#__PURE__*/function () { return fetches[0]; }();
+const fetchPolyfill = /*#__PURE__*/function () { return fetches[1]; }();
 
 export { fetchE as fetch };
 export { fetchPolyfill as fetchP };
